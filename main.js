4cf199a6e7762c1087a1d7106eef12ba04e2781e
Fawwaz1324 <124411333+Fawwaz1324@users.noreply.github.com>
2024-02-10 09:36:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+hp_song ="";
+pp_song ="";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload()
+{
+    hp_song = loadSound("music.mp3");
+    pp_song = loadSong("music2.mp3");
+}
+
+function setup()
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+}
+
+function play()
+{
+    hp_song.play();
+    hp_song.setVolume(1);
+    hp_song.rate(1);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY); 
+    }
+}
